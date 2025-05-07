c9ea737868b55845149b305dbeeb84b83c72bde4
Shkettoe <denis.tosic@proton.me>
2025-05-07 16:27:40 +0200

    login and register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,32 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useActionState } from "react";
+import { login } from "./actions";
 
 const Login = () => {
-  const submittedAction = async (data) => {
-    console.log("submitted", data);
-  };
+  const [state, action, pending] = useActionState(login, null);
+
   return (
     <div className="w-full max-w-md mx-auto">
-      <form action={submittedAction} className="flex flex-col gap-2">
+      <form action={action} className="flex flex-col gap-2">
         <input type="email" name="email" placeholder="Email" />
+        {state?.errors?.email && (
+          <p className="text-red-500">{state.errors.email[0]}</p>
+        )}
         <input type="password" name="password" placeholder="********" />
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded-md">
+        {state?.errors?.password && (
+          <p className="text-red-500">{state.errors.password[0]}</p>
+        )}
+        <button
+          disabled={pending}
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded-md disabled:opacity-35 disabled:cursor-not-allowed"
+        >
           Log in
         </button>
+        {typeof state?.error === "string" && (
+          <p className="text-red-500">{state.error}</p>
+        )}
         <p>
           Don&apos;t have an account?{" "}
           <Link href={"register"} className="text-blue-500">
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,18 +1,59 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useActionState } from "react";
+import { register } from "./actions";
 
 const Register = () => {
+  const [state, action, pending] = useActionState(register, null);
+
   return (
     <div className="w-full max-w-md mx-auto">
-      <form action="" className="flex flex-col gap-2">
-        <input type="text" name="firstName" placeholder="John" />
-        <input type="text" name="lastName" placeholder="Doe" />
-        <input type="email" name="email" placeholder="john@doe.com" />
+      <form action={action} className="flex flex-col gap-2">
+        <input
+          type="text"
+          name="firstName"
+          placeholder="John"
+          defaultValue={!state?.errors?.firstName ? state?.data.firstName : ""}
+        />
+        {state?.errors?.firstName && (
+          <p className="text-red-500">{state?.errors.firstName}</p>
+        )}
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Doe"
+          defaultValue={!state?.errors?.lastName ? state?.data.lastName : ""}
+        />
+        {state?.errors?.lastName && (
+          <p className="text-red-500">{state?.errors.lastName}</p>
+        )}
+        <input
+          type="email"
+          name="email"
+          placeholder="john@doe.com"
+          defaultValue={!state?.errors?.email ? state?.data.email : ""}
+        />
+        {state?.errors?.email && (
+          <p className="text-red-500">{state?.errors.email}</p>
+        )}
         <input type="password" name="password" placeholder="********" />
+        {state?.errors?.password && (
+          <p className="text-red-500">{state?.errors.password}</p>
+        )}
         <input type="password" name="confirmPassword" placeholder="********" />
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded-md">
+        {state?.errors?.confirmPassword && (
+          <p className="text-red-500">{state?.errors.confirmPassword}</p>
+        )}
+        <button
+          disabled={pending}
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded-md disabled:opacity-30 disabled:cursor-not-allowed"
+        >
           Register
         </button>
+        {typeof state?.error === "string" && (
+          <p className="text-red-500">{state.error}</p>
+        )}
         <p>
           Already have an account?{" "}
           <Link href={"login"} className="text-blue-500">
